935d2ffa01c6087b5f018e6acfd8c582054d3f19
aditiadya <aditiadya1211@gmail.com>
2024-07-03 23:20:23 +0530

    tutorial4 added

---
+++ tutorial4/app.js
@@ -0,0 +1,145 @@
+function hello(){
+    console.log("hello");
+}
+hello();
+
+//rolling dice
+function rollDice(){
+    let rand = Math.floor(Math.random()*6)+1;
+    console.log(rand);
+}
+rollDice();
+rollDice();
+rollDice();
+rollDice();
+
+//arguments
+function printInfo(name, age){
+    console.log(`${name}'s age is ${age}.`);
+}
+printInfo("aditi", 21);
+printInfo("aditi");
+
+//return keyword
+function sum(a,b){
+    return a+b;
+}
+let s = sum(2,5);
+console.log(s);
+
+//concat all strings
+// let str = ["hi", "hello", "bye", "!"];
+// function concat(str){
+//     let result = "";
+//     for(let i=0;i<str.length();i++){
+//         result+=str[i];
+//     }
+//     return result;
+// }
+
+//function expression
+let sum2 = function sum(a,b){
+    return a+b;
+}
+console.log(sum(1,2));
+
+//higher order functions 
+
+//1) takes one or more functions as arguments 
+function multipleGreet(func, n){
+    for(let i=0;i<=n;i++){
+        func();
+    }
+}
+let greet = function(){
+    console.log("hello");
+}
+multipleGreet(greet,20);
+
+//2) returns a function.
+function oddOrEven(request){
+    if(request=="odd"){
+        let odd = function(n){
+            console.log(n%2!=0);    //returns odd function
+        }
+        return odd;
+    }
+    else if(request=="even"){
+        let even = function(n){
+            console.log(n%2==0);    //returns even function
+        }
+        return even;
+    }
+    else{
+        console.log("Wrong request");   
+    }
+}
+let request = "odd";
+let func = oddOrEven(request);
+func(3);
+func(4);
+
+//methods : actions that can be performed on an object
+const calculator = {
+    add : function(a, b){
+        return a+b;
+    },
+    sub : function(a, b){
+        return a-b;
+    },
+    mul : function(a, b){
+        return a*b;
+    }
+}
+console.log(calculator.add(5,6));
+
+const calculator2 = {
+    add(a, b){
+        return a+b;
+    },
+    sub(a, b){
+        return a-b;
+    },
+    mul(a, b){
+        return a*b;
+    }
+}
+console.log(calculator.mul(5,6));
+
+//this keyword : refers to an object that is executing the current piece of code
+const student = {
+    name: "aditi",
+    age: 21,
+    eng: 94.4,
+    phy: 77,
+    math:81,
+    getAvg(){
+        let avg = (this.eng + this.phy + this.math)/3;
+        console.log(avg);
+    }
+};
+student.getAvg();
+
+//try and catch statements
+//try : allows you to define a piece of code to be tested for errors while it is being executed
+//catch : allows you to define a block of code to be executed, if an error occurs in the try block
+try{
+    console.log(a);
+}
+catch(e){
+    console.log("Caught an error. Variable 'a' does not exist.")
+    console.log(e);
+}
+
+//arrow function
+const add = (a, b) => {
+    console.log(a+b);
+}
+add(2,5);
+
+//implicit return in arrow functions
+const mul = (a, b) => (a*b);
+console.log(mul(2,5));
+
+//set timeout function 
+console.log("hi there");
