0c9b5d1251e83f313299e24a5b606212512d2713
aditiadya <aditiadya1211@gmail.com>
2024-07-11 04:39:06 +0530

    tutorial 6 added

--- tutorial6/app.js
+++ tutorial6/app.js
@@ -19,3 +19,43 @@ changeColor("red", 1000, ()=>{
         });
     });
 });
+
+//promise chaining for above code
+function changeColor(color, delay){
+    return new Promise((resolve, reject)=> {
+        setTimeout(()=> {
+            h1.style.color = color;
+            resolve("color changed");
+        }, delay);
+    })
+}
+changeColor("red", 1000)  //req-> promise object
+    .then((result) => {
+        console.log("result : ", result);
+        return changeColor("green", 1000)
+    })
+    .then((result) => {
+        console.log("result : ", result);
+        return changeColor("purple", 1000)
+    })
+    .then((result) => {
+        console.log("result : ", result);
+        return changeColor("yellow", 1000)
+    })
+    .then((result) => {
+        console.log("result : ", result);
+        return changeColor("blue", 1000)
+    })
+    .then((result) => {
+
+        console.log("result : ", result);
+        return changeColor("orange", 1000)
+    })
+    .then((result) => {
+
+        console.log("result : ", result);
+        return changeColor("black", 1000)
+    })
+    .catch((error) => {
+        console.log("some promise was rejected");
+    });
