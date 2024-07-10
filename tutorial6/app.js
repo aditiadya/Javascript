8fad51241fdf603ceae279593ec6753afdaff987
aditiadya <aditiadya1211@gmail.com>
2024-07-11 05:09:23 +0530

    tutorial 6 updated

--- tutorial6/app.js
+++ tutorial6/app.js
@@ -1,26 +1,75 @@
 //callback hell -> callback nesting
+
+// let h1 = document.querySelector("h1");
+
+// function changeColor(color, delay, nextColor){
+//     setTimeout(()=> {
+//         h1.style.color = color;
+//         nextColor();
+//     }, delay);
+// }
+
+// changeColor("red", 1000, ()=>{
+//     changeColor("green", 1000, ()=>{
+//         changeColor("purple", 1000, ()=>{
+//             changeColor("yellow", 1000, ()=>{
+//                 changeColor("blue", 1000, ()=>{
+//                     changeColor("orange", 1000);
+//                 });
+//             });
+//         });
+//     });
+// });
+
+//************************************************************************************************************
+
+// //promise chaining for above code
+
+// let h1 = document.querySelector("h1");
+// function changeColor(color, delay){
+//     return new Promise((resolve, reject)=> {
+//         setTimeout(()=> {
+//             h1.style.color = color;
+//             resolve("color changed");
+//         }, delay);
+//     })
+// }
+// changeColor("red", 1000)  //req-> promise object
+//     .then((result) => {
+//         console.log("result : ", result);
+//         return changeColor("green", 1000)
+//     })
+//     .then((result) => {
+//         console.log("result : ", result);
+//         return changeColor("purple", 1000)
+//     })
+//     .then((result) => {
+//         console.log("result : ", result);
+//         return changeColor("yellow", 1000)
+//     })
+//     .then((result) => {
+//         console.log("result : ", result);
+//         return changeColor("blue", 1000)
+//     })
+//     .then((result) => {
+
+//         console.log("result : ", result);
+//         return changeColor("orange", 1000)
+//     })
+//     .then((result) => {
+
+//         console.log("result : ", result);
+//         return changeColor("black", 1000)
+//     })
+//     .catch((error) => {
+//         console.log("some promise was rejected");
+//     });
+
+//************************************************************************************************************
+
+//async and await
 let h1 = document.querySelector("h1");
 
-function changeColor(color, delay, nextColor){
-    setTimeout(()=> {
-        h1.style.color = color;
-        nextColor();
-    }, delay);
-}
-
-changeColor("red", 1000, ()=>{
-    changeColor("green", 1000, ()=>{
-        changeColor("purple", 1000, ()=>{
-            changeColor("yellow", 1000, ()=>{
-                changeColor("blue", 1000, ()=>{
-                    changeColor("orange", 1000);
-                });
-            });
-        });
-    });
-});
-
-//promise chaining for above code
 function changeColor(color, delay){
     return new Promise((resolve, reject)=> {
         setTimeout(()=> {
@@ -29,33 +78,13 @@ function changeColor(color, delay){
         }, delay);
     })
 }
-changeColor("red", 1000)  //req-> promise object
-    .then((result) => {
-        console.log("result : ", result);
-        return changeColor("green", 1000)
-    })
-    .then((result) => {
-        console.log("result : ", result);
-        return changeColor("purple", 1000)
-    })
-    .then((result) => {
-        console.log("result : ", result);
-        return changeColor("yellow", 1000)
-    })
-    .then((result) => {
-        console.log("result : ", result);
-        return changeColor("blue", 1000)
-    })
-    .then((result) => {
-
-        console.log("result : ", result);
-        return changeColor("orange", 1000)
-    })
-    .then((result) => {
-
-        console.log("result : ", result);
-        return changeColor("black", 1000)
-    })
-    .catch((error) => {
-        console.log("some promise was rejected");
-    });
+async function demo2(){
+    await changeColor("red", 1000);
+    await changeColor("green", 1000);
+    await changeColor("purple", 1000);
+    await changeColor("yellow", 1000);
+    await changeColor("blue", 1000);
+    await changeColor("orange", 1000);
+    changeColor("black", 1000);
+}
+demo2();
