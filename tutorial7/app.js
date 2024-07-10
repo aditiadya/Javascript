d739e79ddc7f6f9995d7446c2a2a8aba4510bb3e
aditiadya <aditiadya1211@gmail.com>
2024-07-11 04:56:19 +0530

    tutorial 7 added

--- tutorial7/app.js
+++ tutorial7/app.js
@@ -45,7 +45,7 @@
 //to get rid of the above code which is very lengthy, we use promises
 function savetoDb(data) {
     return new Promise((resolve, reject) => {
-        let internetspeed = Math.floor(Math.random() * 10 + 1);
+        let internetspeed = Math.floor(Math.random() * 10)+1;
         if (internetspeed > 4) {
             resolve("data was saved.");
         }
